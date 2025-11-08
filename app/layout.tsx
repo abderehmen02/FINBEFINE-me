9bc93ca571511694a6067dc95fcb37a58d4b1b65
abderrahmane <ilahalaha0987@gmail.com>
2025-11-08 01:11:15 +0100

    implementing the font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./globals.css";
+import { cn } from "@/lib/tailwind";
+import { montserrat } from "@/fonts/Montserrat";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(montserrat.className, montserrat.variable, "antialiased")}
       >
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
---
+++ icons/analytics.tsx
@@ -0,0 +1,13 @@
+export const AnalyticsIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="30" height="30" rx="5" fill="#5279F4" />
+    </svg>
+  );
+};
